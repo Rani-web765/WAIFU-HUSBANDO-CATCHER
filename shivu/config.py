6aa1c6492546ba3b294166584b0b32b86eaa26b8
Rani-web765 <pragyananddhakad3@gmail.com>
2024-12-20 14:30:23 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6584789596"
-    sudo_users = "6101457748", "5932230962", "6100011620", "6154972031", "6584789596", "5702598840"
+    OWNER_ID = "7526369190"
+    sudo_users = "7526369190", "5884969921", "6961287189"
     GROUP_ID = -1002133191051
     TOKEN = "6462620320:AAGIBSjD_PmyiSNPmF3kdYsO0G2TSMpQe9M"
     mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
-    SUPPORT_CHAT = "Grabbing_Your_WH_Group"
-    UPDATE_CHAT = "Grabbing_Your_WH_Channel"
-    BOT_USERNAME = "Grabbing_Your_Waifu_Bot"
-    CHARA_CHANNEL_ID = "-1002009998662"
+    SUPPORT_CHAT = "waifexanime"
+    UPDATE_CHAT = "waifexanime"
+    BOT_USERNAME = "Collectionwaife_bot"
+    CHARA_CHANNEL_ID = "-1002119873436"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
