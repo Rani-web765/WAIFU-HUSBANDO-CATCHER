46cbdc718ce8e7b7a802cb9c09c92ce7b812061e
Rani-web765 <pragyananddhakad3@gmail.com>
2024-12-21 22:14:45 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "7526369190"
     sudo_users = "7526369190", "5884969921", "6961287189"
     GROUP_ID = -1002133191051
-    TOKEN = "6462620320:AAGIBSjD_PmyiSNPmF3kdYsO0G2TSMpQe9M"
+    TOKEN = "7367511450:AAG-ioIEAyaKfnIRi43NbiRjafQqbJmiBQU"
     mongo_url = "mongodb+srv://Srikanta:srikanta@cluster0.xzbil3m.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
     SUPPORT_CHAT = "waifexanime"
